dfc621d88b5ddc9410da1bde40394fc20e7d16b8
Владислав Гречанник <m2108502@edu.misis.ru>
2023-05-22 23:41:59 +0300

    Start work on buttons under the banner

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,7 +1,19 @@
 import Banner from "./MainPage.Banner";
+import styles from "./MainPage.module.css";
+import {useCallback, useState} from "react";
 
 export default function Main() {
+    const [isOnline, setOnline] = useState(false);
+    const setOnlineTrue = useCallback(() => {setOnline(true)}, [setOnline]);
+    const setOnlineFalse = useCallback(() => {setOnline(false)}, [setOnline]);
+
     return <div>
         <Banner/>
+        <div className={styles.picker}>
+            <div className={styles.online_picker}>
+                <div className={!isOnline ? styles.active : undefined} tabIndex={0} onClick={setOnlineFalse}>Очно</div>
+                <div className={isOnline ? styles.active : undefined} tabIndex={0} onClick={setOnlineTrue}>Онлайн</div>
+            </div>
+        </div>
     </div>
 }
