e8c4c8f6a7069e8cd44dfa631e22b200c3456eca
Vladislav <vgrechannik@gmail.com>
2023-05-23 03:01:09 +0300

    Make text size and font apply everywhere

--- src/pages/main/components/Banner.tsx
+++ src/pages/main/components/Banner.tsx
@@ -7,8 +7,8 @@ import SearchBar from "./SearchBar";
 export default function Banner() {
     return <div className={[styles.banner, styles.flex_row].join(" ")}>
         <div className={[styles.flex_column, styles.banner_flavour_text].join(" ")}>
-            <div className={styles.flex_row}><p>Эти&nbsp;</p><p className={styles.inverse_color_text_upper}>&nbsp;мероприятия&nbsp;</p></div>
-            <div className={styles.flex_row}><p>могут вас&nbsp;</p><p className={styles.inverse_color_text_lower}>&nbsp;заинтересовать!&nbsp;</p></div>
+            <div className={styles.flex_row}><p className={styles.big_text}>Эти&nbsp;</p><p className={[styles.inverse_color_text_upper, styles.big_text].join(" ")}>&nbsp;мероприятия&nbsp;</p></div>
+            <div className={styles.flex_row}><p className={styles.big_text}>могут вас&nbsp;</p><p className={[styles.inverse_color_text_lower, styles.big_text].join(" ")}>&nbsp;заинтересовать!&nbsp;</p></div>
             <p className={styles.how_does_it_work}>Как это работает?</p>
             <SearchBar/>
         </div>
