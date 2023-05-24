f83da529a23c5972c1da499a2300e3ffde554ae5
Vladislav <vgrechannik@gmail.com>
2023-05-25 01:24:06 +0300

    Add footer

--- src/AfterLogin.tsx
+++ src/AfterLogin.tsx
@@ -2,14 +2,16 @@ import {LoginInfo} from "./App";
 import NavBar from "./NavBar";
 import {useState} from "react";
 import {Outlet} from "react-router-dom";
+import Footer from "./Footer";
 
 export default function AfterLogin(props: {user: LoginInfo}) {
     const [blind, setBlind] = useState(false);
 
     return <div>
-        <NavBar forBlind={blind} setForBlind={setBlind}></NavBar>
+        <NavBar forBlind={blind} setForBlind={setBlind}/>
         <main>
             <Outlet/>
         </main>
+        <Footer/>
     </div>;
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,31 @@
+import styles from "./Footer.module.css";
+import Logo from "/footer-logo.png";
+import Vk from "./assets/icons/vk.svg";
+import Ok from "./assets/icons/ok.svg";
+import Tg from "./assets/icons/telegram.svg";
+
+function LinkToSocial({logo, text, link}: { logo: string, text: string, link: string }) {
+    return <a href={link} className={styles.link_to_social}>
+        <img src={logo} alt={""}/>
+        <p>{text}</p>
+    </a>
+}
+
+export default function Footer() {
+    return <footer className={styles.footer}>
+        <img src={Logo} alt={"Московское долголетие"} className={styles.logo}/>
+        <div className={styles.links}>
+            <LinkToSocial logo={Vk} text={"Мы ВКонтакте"} link={"https://vk.com/mosdolgoletie"}/>
+            <LinkToSocial logo={Ok} text={"Мы в Одноклассниках"} link={"https://ok.ru/mosdolgoletie"}/>
+            <LinkToSocial logo={Tg} text={"Мы в Телеграмме"} link={"https://t.me/mosdolgoletie"}/>
+        </div>
+        <label className={styles.contacts} aria-label={"Контакты"}>
+            <h1>Контакты</h1>
+            <ul aria-label={"Контакты"}>
+                <li aria-label={"Телефон"}>Телефон: 79128079999</li>
+                <li aria-label={"Почта"}>Почта: dolgojit100_1@mos.ru</li>
+                <li aria-label={"Адрес"}>Адрес: улица Здоровья, дом 3, корпус 1</li>
+            </ul>
+        </label>
+    </footer>
+}
