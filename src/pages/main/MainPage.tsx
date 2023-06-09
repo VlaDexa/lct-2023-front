73ee11082fa55be2d1ffff4cc594f15d934d9b42
Vladislav <vgrechannik@gmail.com>
2023-06-09 18:00:38 +0300

    Change an endpoint

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -38,7 +38,7 @@ function pageFilter(filters: Filters, groups: Group[]) {
 }
 
 async function getGroups(): Promise<Group[]> {
-    const is_old = await RecsService.isExistRecsApiV1RecsIsExistGet();
+    const is_old = await RecsService.isRecsExistApiV1RecsIsRecsExistGet();
     const groups_nums: number[] = await RecsService.giveRecsApiV1RecsGet(!is_old)
 
     const groups: Group[] = (
